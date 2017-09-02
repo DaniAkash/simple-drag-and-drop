d2c62bf44bb273fcd7a76208a4a69106483bf65f
DaniAkash <s.daniakash@gmail.com>
2017-09-02 11:30:12 +0530

    create container

--- src/Components/Row/Row.js
+++ src/Components/Row/Row.js
@@ -1,8 +1,18 @@
 import React from 'react';
 
-const Row = () => (
-    <div>
-        <h1 style={{color: 'white'}}>SELECTED</h1>
+import SelectedTile from '../Tile/SelectedTile';
+
+const Row = ({items}) => (
+    <div className={'row'}>
+        {
+            items.map((each, index) => {
+                if(each === 1) {
+                    return <SelectedTile key={index}/>;
+                } else {
+                    return null;
+                }
+            })
+        }
     </div>
 );
 
---
+++ src/Components/Tile/SelectedTile.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const selectedTile = () => (
+    <div className={'selected-tile'} draggable={true}/>
+)
+
+export default selectedTile;
